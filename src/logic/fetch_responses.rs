ace2bd830565d1c79f8166bc971d9f8b6c343c42
Vojtěch Toman <cry.wolf@centrum.cz>
2024-11-02 22:34:25 +0100

    Fetch messages for multiple topics across multiple partitions

--- src/logic/fetch_responses.rs
+++ src/logic/fetch_responses.rs
@@ -21,47 +21,54 @@ pub fn process(req: FetchRequestV16) -> Result<FetchResponseV16> {
         ));
     };
 
-    let responses = if let Some(topic_request) = req.topics.first() {
-        // TODO iterate through more topic requests?
+    let mut responses = Vec::new();
 
+    // iterate through all requested topics
+    for topic_request in req.topics {
         // default - topic does not exist
         let mut error_code = ErrorCode::UnknownTopicId;
 
         let topic_id = topic_request.topic_id.clone();
 
-        let partition_id = topic_request
-            .partitions
-            .first()
-            .expect("partition_id shoud be set in request")
-            .partition;
+        // iterate through requested partitions for the topic
+        let mut partitions = Vec::new();
+        for partition in topic_request.partitions {
+            let partition_id = partition.partition;
 
-        let record_batches = RecordBatches::from_file(CLUSTER_METADATA_LOG_FILE)
-            .context("read record batches from file")?;
+            let record_batches = RecordBatches::from_file(CLUSTER_METADATA_LOG_FILE)
+                .context("read record batches from file")?;
 
-        let mut partition_record_batches = Vec::new();
-        if let Some(raw_batch) = record_batches.raw_batch_for_topic(&topic_id, partition_id)? {
-            error_code = ErrorCode::None;
-            let batch_bytes = BatchBytes { bytes: raw_batch };
-            partition_record_batches.push(batch_bytes);
+            let mut partition_record_batches = Vec::new();
+            if let Some(raw_batch) = record_batches
+                .raw_batch_for_topic(&topic_id, partition_id)
+                .with_context(|| {
+                    format!(
+                        "read messages for topic '{}' in partition '{}'",
+                        topic_id, partition_id
+                    )
+                })?
+            {
+                error_code = ErrorCode::None;
+                let batch_bytes = BatchBytes { bytes: raw_batch };
+                partition_record_batches.push(batch_bytes);
+            }
+
+            let partition = TopicPartition {
+                partition_index: 0,
+                error_code,
+                high_watermark: 0,
+                last_stable_offset: 0,
+                log_start_offset: 0,
+                aborted_transactions: Vec::new(),
+                preferred_read_replica: 0,
+                record_batches: partition_record_batches,
+            };
+            partitions.push(partition);
         }
 
-        let partition = TopicPartition {
-            partition_index: 0,
-            error_code,
-            high_watermark: 0,
-            last_stable_offset: 0,
-            log_start_offset: 0,
-            aborted_transactions: Vec::new(),
-            preferred_read_replica: 0,
-            record_batches: partition_record_batches,
-        };
-        let partitions = vec![partition];
         let topic_response = TopicResponse::new(topic_id, partitions);
-        vec![topic_response]
-    } else {
-        // no topic requested
-        vec![]
-    };
+        responses.push(topic_response);
+    }
 
     Ok(FetchResponseV16::new(
         req.header.correlation_id,
